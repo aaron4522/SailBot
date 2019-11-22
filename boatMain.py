89d6f3b5845fcde361407d73b8b464bdc4381a1d
Thomas-D-Pitt <54122854+Thomas-D-Pitt@users.noreply.github.com>
2019-11-21 20:19:03 -0500

    Add files via upload

---
+++ boatMain.py
@@ -0,0 +1,69 @@
+import constants as C
+
+from windvane import windVane
+from GPS import Gps 
+from drivers import driver
+from transceiver import arduino
+
+class boat:
+
+    def __init__(self):
+
+        self.gps = Gps()
+        self.windvane = windVane()
+        self.drivers = driver()
+        self.arduino = arduino(C.ARDU_PORT)
+
+        self.currentTarget = None # (longitude, latitude) tuple
+        self.targets = [] # holds (longitude, latitude) tuples
+
+    def move(self):
+        if self.gps.distanceTo(currentTarget) < C.ACCEPTABLE_RANGE and len(self.targets) > 0:
+            #next target
+
+            if (False):#check if next target would involve no-go-zone, if so set current target to another point for zig-zag path
+                pass
+
+            else:
+                self.currentTarget = self.targets.pop(0)
+
+        else:
+            #move towards target
+            self.adjustSail()
+            self.adjustRudder()
+            
+    def adjustSail(self):
+        if self.currentTarget:
+            #adjust sail for best wind
+            targetAngle = ???
+            self.drivers.sail.set(targetAngle)
+        else:
+            #move sail to home position
+            self.drivers.sail.set(0)
+            
+    def adjustRudder(self):
+        if self.currentTarget:
+            #adjust rudder for best wind
+            angleTo = gps.angleTo(self.currentTarget)
+            d_angle = angleTo - gps.track_angle_deg
+
+            if d_angle > 180: d_angle -= 180
+
+            self.drivers.rudder.set(d_angle)
+            
+        else:
+            #move sail to home position
+            self.drivers.rudder.set(0)
+
+    def readMessages(self):
+        msgs = self.arduino.read()
+
+        for msg in msgs:
+            ary = msg.split(' ')
+            if len(ary) > 1:
+                if ary[0] == 'sail': self.drivers.sail.set(float(ary[1]))
+                elif ary[0] == 'rudder': self.drivers.rudder.set(float(ary[1]))
+                elif ary[0] == 'mode': print("TODO: add Modes")
+
+boat()
+
---
+++ windvane.py
@@ -0,0 +1,57 @@
+from RPi import GPIO
+from time import sleep
+
+class windVane():
+
+	def __init__(self):
+
+		self.stepsPerRev = 256
+
+		self.clk = 17
+		self.dt = 18
+
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.clk, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+		GPIO.setup(self.dt, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+
+		self.counter = 0
+		self.clkLastState = GPIO.input(self.clk)
+
+		pump_thread = Thread(target=self.run)
+		pump_thread.start()
+
+	def map(self, x, min1, max1, min2, max2):
+   		x = min(max(x, min1), max1)
+    	return min2 + (max2-min2)*((x-min1)/(max1-min1))
+
+	@property
+	def angle(self):
+		counter = self.counter
+
+		while (counter < 0):
+			counter += self.stepsPerRev
+
+		counter = counter % self.stepsPerRev
+		return self.map(counter, 0, stepsPerRev-1, 0, 359)
+
+	
+
+	def run(self):
+		try:
+			while True:
+				self.update()
+
+		finally:
+			GPIO.cleanup()
+
+	def update(self):
+		clkState = GPIO.input(clk)
+		dtState = GPIO.input(dt)
+		if clkState != clkLastState:
+
+			if dtState != clkState:
+				counter += 1
+			else:
+				counter -= 1
+
+			clkLastState = clkState
