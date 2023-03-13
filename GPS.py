fa0392e5c93f74d14298db165e3b28f5f11a635f
Thomas <tad85@pitt.edu>
2023-03-13 19:59:21 -0400

    ros fixes

--- GPS.py
+++ GPS.py
@@ -13,7 +13,11 @@ import adafruit_lsm303dlh_mag
 from time import sleep
 from threading import Thread
 import math
-from sailbot.boatMath import degreesToRadians, getCoordinateADistanceAlongAngle, distanceInMBetweenEarthCoordinates, computeNewCoordinate, angleBetweenCoordinates, convertDegMinToDecDeg, convertWindAngle
+
+try:
+    from boatMath import degreesToRadians, getCoordinateADistanceAlongAngle, distanceInMBetweenEarthCoordinates, computeNewCoordinate, angleBetweenCoordinates, convertDegMinToDecDeg, convertWindAngle
+except:
+    from sailbot.boatMath import degreesToRadians, getCoordinateADistanceAlongAngle, distanceInMBetweenEarthCoordinates, computeNewCoordinate, angleBetweenCoordinates, convertDegMinToDecDeg, convertWindAngle
 
 import rclpy
 from rclpy.node import Node
