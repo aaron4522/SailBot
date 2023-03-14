7b81a823739aedf4a4c2a11a1fb63b465bde1018
Thomas <tad85@pitt.edu>
2023-03-13 20:17:00 -0400

    ros fixes

--- constants.py
+++ constants.py
@@ -1,7 +1,13 @@
 # reads values from config.ini and returns them
 import configparser
+
+try:
+    import boatMath # see if we are in sailbot folder or parent folder
+    prefix = ''
+except:
+    prefix = 'sailbot/'
 config = configparser.ConfigParser()
-config.read('config.ini')
+config.read(F'{prefix}config.ini')
     
 def save():
     with open('../config.ini', 'w') as configfile:
