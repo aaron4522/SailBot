c6d8404843106bcfeaf412a491b74986a493c021
aaron4522 <rex4522@gmail.com>
2023-04-20 19:00:07 -0400

    Update cameraServos.py
    - cameraServos now uses config
    - code now waits until servos in position! (likely needs multitprocessing now, but solves issue with taking survey pictures before servo in position)

--- cameraServos.py
+++ cameraServos.py
@@ -3,6 +3,8 @@ Drivers and interface for camera servos
 """
 # Code adapted from https://github.com/ArduCAM/PCA9685
 import logging
+from time import sleep
+from math import abs
 
 import constants as c
 try:
@@ -11,18 +13,16 @@ except ImportError as e:
     print("Failed to import some modules, if this is not a simulation fix this before continuing")
     print(f"Exception raised: {e}")
    
-
-
 # Yaw and Pitch assumed to have same range limits
-MAX_ANGLE = 180
-MIN_ANGLE = 0
+MIN_ANGLE = int(c.config(["CAMERASERVOS"]["min_angle"]))
+MAX_ANGLE = int(c.config(["CAMERASERVOS"]["max_angle"]))
 
-DEFAULT_ANGLE = 90
+DEFAULT_ANGLE = int(c.config(["CAMERASERVOS"]["default_angle"]))
 
 # Servo connection ports, if inputs are reversed then switch
 # If servos don't move try setting ports to 2 and 3
-PITCH_PORT = 0
-YAW_PORT = 1
+PITCH_PORT = int(c.config(["CAMERASERVOS"]["pitch_port"]))
+YAW_PORT = int(c.config(["CAMERASERVOS"]["yaw_port"]))
 
 
 class CameraServos():
@@ -64,8 +64,12 @@ class CameraServos():
         elif angle > MAX_ANGLE:
             self._kit.servo[PITCH_PORT].angle = MAX_ANGLE
         else:
+            logging.debug(f"Moving camera pitch to {angle}")
             self._kit.servo[PITCH_PORT].angle = angle
-            logging.debug("Moving camera pitch to {angle}")
+            
+            while (abs(self.pitch - angle) < 0.1):
+                sleep(0.1)
+            logging.debug(f"Camera pitch set to {angle}")
             
     @property
     def yaw(self):
@@ -78,8 +82,12 @@ class CameraServos():
         elif angle > MAX_ANGLE:
             self._kit.servo[YAW_PORT].angle = MAX_ANGLE
         else:
+            logging.debug(f"Moving camera yaw to {angle}") 
             self._kit.servo[YAW_PORT].angle = angle
-            logging.debug("Moving camera yaw to {angle}")     
+            
+            while (abs(self.yaw - angle) < 0.1):
+                sleep(0.1)
+            logging.debug(f"Camera yaw set to {angle}")
     
 if __name__ == "__main__":
     servos = CameraServos()
